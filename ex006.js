d26b17a0924d4bd080d949586fb53d7bc61edfae
alvaro lopes <alvim.lopez@ gmail.com>
2021-10-25 21:22:39 -0300

    criando uma calculadora de velocidade

---
+++ ex006.js
@@ -0,0 +1,8 @@
+console.log('funcionando')
+var vel = 50
+console.log(`a levocidade e de ${vel}km`)
+if(vel > 60) {
+    console.log(`voce ultrapassou a velocidade`)
+} else {
+    console.log (`dirija com cinto de segurança`)
+}
