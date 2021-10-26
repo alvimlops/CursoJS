6b7b382104f62ae9d4d6cc81d61077e7fae9fa54
alvaro lopes <alvim.lopez@ gmail.com>
2021-10-26 19:04:06 -0300

    criando exercicios de condições

---
+++ ex009.js
@@ -0,0 +1,10 @@
+var idade = 22
+if (idade < 16){
+    console.log('não vota');
+} else {
+    if(idade < 18){
+        console.log('Voto Opcional')
+    } else {
+        console.log ('voto obrigatorio')
+    }
+}
